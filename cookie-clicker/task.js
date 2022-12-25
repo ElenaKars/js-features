8222953fd3067c39616586fed7f3ba8886881e89
ElenaKars <87471171+ElenaKars@users.noreply.github.com>
2022-12-25 11:42:49 +0800

    done (with bonus task)

---
+++ cookie-clicker/task.js
@@ -0,0 +1,17 @@
+'use strict'
+
+let clickerCounterEl = document.getElementById('clicker__counter')
+
+const cookieImgEl = document.getElementById('cookie')
+let clickerCounterSpeedEl = document.getElementById('clicker__counter_speed')
+
+cookieImgEl.addEventListener('click', function () {
+  clickerCounterEl.textContent++
+  clickerCounterSpeedEl.textContent = new Date().getMilliseconds() / 100
+
+  if (clickerCounterEl.textContent % 2 !== 0) {
+    cookieImgEl.width = 300
+  } else {
+    cookieImgEl.width = 200
+  }
+})
